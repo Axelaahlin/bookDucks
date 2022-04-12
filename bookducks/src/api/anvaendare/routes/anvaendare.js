94ec0105905a96ab4161b36d1bb85335d55e08a4
axelaahlin <axel.ahlinandersson@yh.nackademin.se>
2022-04-12 12:43:18 +0200

    save

--- bookducks/src/api/anvaendare/routes/anvaendare.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * anvaendare router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::anvaendare.anvaendare');
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let booklist = document.querySelector(".allbooks")
+let ljudbokslist = document.querySelector(".allljudboks")
 let loggInBtn = document.querySelector(".loggInBtn")
 let loggInView = document.querySelector(".loggin")
 let mainview = document.querySelector(".main")
@@ -6,17 +7,36 @@ let registreraATagg = document.querySelector(".registrera")
 let registerView = document.querySelector(".registerView")
 let h1 = document.querySelector("#h1")
 let logginBtn = document.querySelector("#logginBtn")
+let header = document.querySelector(".header")
 let headerDiv = document.querySelector(".headerDiv")
+let minasidorView = document.querySelector(".minasidorView")
+let addljudbokview = document.querySelector(".addljudbokview")
+let addbokview = document.querySelector(".addbokview")
 
-let fetch = async (url) => {
-let response = await axios.get(url)
-let data = response.data.data
-console.log(data)
+
+
+let registerbtn = document.querySelector("#registerbtn")
+
+let registerUser = async () => {
+  let registerUsername = document.querySelector("#reganvändarnamn")
+  let registerEmail = document.querySelector("#regEmail")
+  let registerPassword = document.querySelector("#regPassword") 
+  console.log(registerPassword.value);
+
+  await axios.post('http://localhost:1337/api/auth/local/register', {
+          username: registerUsername.value,
+          email: registerEmail.value,
+          password: registerPassword.value,
+    });
 }
 
-let renderBooks = async () => {
-let response = await axios.get("http://localhost:1337/api/books?populate=*");
+
+let renderBooks = async (vart) => {
+let response = await axios.get(`http://localhost:1337/api/books?populate=*`);
 let books = response.data.data
+addljudbokview.style.display = "none"
+addbokview.style.display = "none"
+
 
 books.forEach(book => {
   // console.log(book.attributes.genrer.data.attributes.genrer)
@@ -30,8 +50,8 @@ books.forEach(book => {
   <p>Genrer: ${book.attributes.genrer.data.attributes.genrer}</p>
   <p>Betyg: ${book.attributes.betyg}/10 </p>
   <div class="utlåning">
-  <p>Lånas ut av: ${book.attributes.anvandare.data.attributes.Username}</p>
-  <p>${book.attributes.anvandare.data.attributes.Email}</p>
+  <p>Lånas ut av: ${book.attributes.user.data.attributes.username}</p>
+  <p>${book.attributes.user.data.attributes.email}</p>
   <div>
   </div>
   
@@ -40,10 +60,97 @@ books.forEach(book => {
   if(book.attributes.omslag.data){
     image.src = "http://localhost:1337" + book.attributes.omslag.data[0].attributes.url;
   }
-  booklist.append(li)
+  vart.append(li)
   li.prepend(image)
   });
 
+} 
+let renderPrivateBooks = async (vart) => {
+  let response = await axios.get(`http://localhost:1337/api/books?populate=*`);
+  let books = response.data.data
+  let username = sessionStorage.getItem("username")
+
+  books.forEach(book => {
+    if(book.attributes.user.data.attributes.username == username){
+      let li = document.createElement("li");
+      li.innerHTML = 
+      `
+      <div class="bokinfo">
+      <h4>${book.attributes.titel}</h4>
+      <p>Författare: ${book.attributes.forfattare}</p>
+      <p>Antal sidor: ${book.attributes.sidor}</p>
+      <p>Genrer: ${book.attributes.genrer.data.attributes.genrer}</p>
+      <p>Betyg: ${book.attributes.betyg}/10</p>
+      </div>
+      
+      `
+      let image = document.createElement("img")
+      if(book.attributes.omslag.data){
+        image.src = "http://localhost:1337" + book.attributes.omslag.data[0].attributes.url;
+      }
+      vart.append(li)
+      li.prepend(image)
+      };
+    })  
+}
+let renderLjudBooks = async (vart) => {
+  let response = await axios.get(`http://localhost:1337/api/ljudboks?populate=*`);
+  let books = response.data.data
+  
+  books.forEach(book => {
+    // console.log(book.attributes.genrer.data.attributes.genrer)
+    let li = document.createElement("li");
+    li.innerHTML = 
+    `
+    <div class="bokinfo">
+    <h4>${book.attributes.titel}</h4>
+    <p>Utgivningsdatum: ${book.attributes.utgivningsdatum}</p>
+    <p>Längd: ${book.attributes.minuter} minuter</p>
+    <p>Genrer: ${book.attributes.genrer.data.attributes.genrer}</p>
+    <p>Betyg: ${book.attributes.betyg}/10 </p>
+    <div class="utlåning">
+    <p>Lånas ut av: ${book.attributes.user.data.attributes.username}</p>
+    <p>${book.attributes.user.data.attributes.email}</p>
+    <div>
+    </div>
+    
+    `
+    let image = document.createElement("img")
+    if(book.attributes.omslag.data){
+      image.src = "http://localhost:1337" + book.attributes.omslag.data.attributes.formats.thumbnail.url;
+    }
+    vart.append(li)
+    li.prepend(image)
+    });
+  
+} 
+let renderPrivateLjudBooks = async (vart) => {
+    let response = await axios.get(`http://localhost:1337/api/ljudboks?populate=*`);
+    let books = response.data.data
+    let username = sessionStorage.getItem("username")
+  
+    books.forEach(book => {
+      if(book.attributes.user.data.attributes.username == username){
+        let li = document.createElement("li");
+        li.innerHTML = 
+        `
+        <div class="bokinfo">
+        <h4>${book.attributes.titel}</h4>
+        <p>Utgivningsdatum: ${book.attributes.utgivningsdatum}</p>
+        <p>Antal minuter: ${book.attributes.minuter}</p>
+        <p>Genrer: ${book.attributes.genrer.data.attributes.genrer}</p>
+        <p>Betyg: ${book.attributes.betyg}/10</p>
+        </div>
+        
+        `
+        let image = document.createElement("img")
+        if(book.attributes.omslag.data){
+          image.src = "http://localhost:1337" + book.attributes.omslag.data.attributes.formats.thumbnail.url;
+        }
+        vart.append(li)
+        li.prepend(image)
+        };
+      })  
 }
 
 loggInBtn.addEventListener("click", () => {
@@ -60,13 +167,15 @@ if(loggInView.style.display = "none" )  {
 h1.addEventListener("click", () => {
   mainview.style.display = "flex"
   loggInView.style.display = "none"
-  registerView.style.display="none"
+  registerView.style.display = "none"
+  minasidorView.style.display = "none"
 })
 
-renderBooks()
+
+renderBooks(booklist) 
+renderLjudBooks(ljudbokslist)
 
 let login = async () => {
-  console.log("hej")
 
   let username = document.querySelector("#logginAnvändarnamn").value
   let password = document.querySelector("#logginPassword").value;
@@ -78,25 +187,105 @@ let login = async () => {
 
   let token = response.data.jwt;
   sessionStorage.setItem("token", token);
+  sessionStorage.setItem("username", username)
 }
-
-logginBtn.addEventListener("click", () => {
-  login()
-})
 
 let checkIfLoggedIn = () => {
   let token = sessionStorage.getItem("token")
-  if(!token){
-  headerDiv.innerHTML = 
+  if(token){
+  
+  header.innerHTML = 
   `
-  <button id="minasidor">Mina sidor</button>
+  <div>
+  <h1 id="h1">BookDucks</h1>
+  </div>
+  <div id="länkdiv">
+  <button id="addljudbok">Lägg till ljudbok</button>
+  <button id="addbok">Lägg till bok</button>
+  </div>
+  <div class="headerDiv">
+  <button class="minasidor">Mina sidor</button>
+  </div>
   `
-  }
-  let minSidaBtn = document.querySelector("#minasidor")
-
-  minSidaBtn.addEventListener("click", () => {
-    
+  document.querySelector("#addljudbok").addEventListener("click", () => {
+  
   })
-}
+  document.querySelector("#addljudbok").addEventListener("click", () => {
+    mainview.style.display = "none"
+    loggInView.style.display = "none"
+    minasidorView.style.display = "none"
+    registerView.style.display = "none"
+    addljudbokview.style.display = "none"
+    addbokview.style.display = "flex"
+  })
 
+  document.querySelector(".minasidor").addEventListener("click", () => {
+    mainview.style.display = "none"
+    loggInView.style.display = "none"
+    minasidorView.style.display = "flex"
+    registerView.style.display = "none"
+
+  
+  
+    let users = async () => {
+      let response = await axios.get("http://localhost:1337/api/users")
+      let data = response.data
+    
+      data.forEach(user => {
+        let username = sessionStorage.getItem("username")
+        
+        let d = new Date(user.createdAt)
+
+        if(username === user.username) {
+          minasidorView.innerHTML = 
+          `
+          <h2>Min sida</h2>
+          <div class="infowrapper">
+            <div class="minSidaInfo">
+              <h3>Mina uppgifter:</h3>
+              <p>Namn: ${user.username}</p>
+              <p>Email: ${user.email}</p>
+              <p>Ditt ID: ${user.id}</p>
+              <p>Du skapade din användare: ${d.getDate()}-${d.getMonth()}-${d.getYear()}</p>
+            </div>
+            <div id="användarbok">
+              <h3>Dina lånade böcker och ljudböcker:</h3>
+              <ul id="användarböcker"></ul>
+              <ul id="användarljudböcker"></ul>
+            </div>
+          </div>
+          `
+          let användarlistan = document.querySelector("#användarböcker")
+          renderPrivateBooks(användarlistan)
+          let användarljudlistan = document.querySelector("#användarljudböcker")
+          renderPrivateLjudBooks(användarljudlistan)
+          ;
+        }
+      })
+    }
+    users()
+    })
+  }
+}
 checkIfLoggedIn()
+
+
+logginBtn.addEventListener("click", async () => {
+  await login()
+  checkIfLoggedIn()
+
+  mainview.style.display = "flex"
+  loggInView.style.display = "none"
+  minasidorView.style.display = "none"
+  registerView.style.display = "none"
+})
+
+registerbtn.addEventListener("click", () => {
+  registerUser();
+  alert("Din användare är skapad, Logga in på nästa sida")
+
+    mainview.style.display = "flex"
+    loggInView.style.display = "none"
+    minasidorView.style.display = "none"
+    registerView.style.display = "none"
+})
